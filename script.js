37898b4796e149763e266d8bb3bde02d76838fc0
sameermd16 <sameerthemc@gmail.com>
2023-03-31 18:58:37 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const goTopBtn = document.querySelector('.go_top_btn');
 const menuBtn = document.querySelector('.menu_btn');
 const mainMenu = document.querySelector('.main_menu');
+const menuClose = document.querySelector('.menu_close');
 
 menuBtn.addEventListener('click', () => {
-    mainMenu.classList.toggle('show');
+    mainMenu.classList.add('show');
+    menuBtn.style.display = "none";
+    menuClose.style.display = 'block';
+})
+menuClose.addEventListener('click', () => {
+    menuClose.style.display = "none";
+    menuBtn.style.display = 'block';
+    mainMenu.classList.remove('show');
 })
 
 
