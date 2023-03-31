bd2713a8c6396b5288b81220ffc925d5bc4bd150
sameermd16 <sameerthemc@gmail.com>
2023-03-31 17:13:48 +0530

    menu button

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const goTopBtn = document.querySelector('.go_top_btn');
+const menuBtn = document.querySelector('.menu_btn');
+const mainMenu = document.querySelector('.main_menu');
+
+menuBtn.addEventListener('click', () => {
+    mainMenu.classList.toggle('show');
+})
+
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
